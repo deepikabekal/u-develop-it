2bac4266ecb2178f526618663fc95516609cad94
Deepika Bekal <deepika.bekal2@gmail.com>
2021-05-17 00:16:53 +0400

    added query to get all the rows in the table

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,3 @@
 DROP DATABASE IF EXISTS election;
-CREATE DATABASE elections;
-USE elections;
+CREATE DATABASE election;
+USE election;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,19 @@ const app = express();
 //add the express.js middleware
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'pass',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 //route
@@ -14,11 +28,16 @@ app.get('/', (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404)
 })
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
