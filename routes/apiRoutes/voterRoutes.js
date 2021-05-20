871f95090190fdd1887a17625cd712c182ccca80
Deepika Bekal <deepika.bekal2@gmail.com>
2021-05-20 23:11:50 +0400

    get routes for voters

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+const { route } = require('./candidateRoutes');
+
+//get all voters
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+
+    db.query(sql, (err, rows) => {
+        if (err)
+        {
+            res.status(500).json({error : err.message});
+            return;
+        }
+
+        res.json({
+            message : 'Success', 
+            data : rows
+        });
+    });
+});
+
+//get individual voters
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err)
+        {
+            res.status(400).json({error : err.message});
+            return;
+        }
+
+        res.json({
+            message : "success",
+            data : row
+        });
+    });
+});
+
+
+
+module.exports = router;
